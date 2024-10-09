97b3f87a3d17996691656cff04b1513ffb28c7ce
Ninna <gianni.ansaldo@gmail.com>
2024-10-09 19:18:45 -0300

    Inspeccionar archivos

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get '/home', to: 'pages#home'
-  get '/projects', to: 'pages#projects'
-  get '/contact', to: 'pages#contact'
+  get "/home", to: "pages#home"
+  get "/projects", to: "pages#projects"
+  get "/contact", to: "pages#contact"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
